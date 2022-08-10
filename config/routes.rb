54e2456cc1ab1ada58aaec984ca25ab9fa276502
Zeek Pardo <zeekpardo@gmail.com>
2022-08-09 19:25:09 -0700

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+ # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  get "about", to: "about#index"
+  root to: "main#index"
+end
